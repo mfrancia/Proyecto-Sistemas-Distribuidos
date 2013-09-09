bbbdc87675feb54f689cc4e311467afbd4a02f8d
mfrancia <manuelfrancia678@gmail.com>
2013-09-09 01:20:48 -0500

    se agrego el boostrap css y el proyecto de entitties framework con conexion a mysql, esta debe ser la base para el proyecto verificar conexion local

---
+++ MvcEntretenimientoWeb/MvcEntretenimientoWeb/Controllers/RegistroCampoController.cs
@@ -0,0 +1,37 @@
+﻿using MvcEntretenimientoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcEntretenimientoWeb.Controllers
+{
+    public class RegistroCampoController : Controller
+    {
+        //
+        // GET: /RegistroCompra/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegistrarCampo()
+        {
+            
+            return View();
+        }
+
+        public JsonResult GetServicios()
+        {
+            ent_familiarEntities db = new ent_familiarEntities();
+            List<servicio> servicio = (from s in db.servicio
+                            select s).ToList();
+
+            return Json(servicio);
+        }
+
+
+    }
+}
